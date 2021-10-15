a19dc30c9399248bed58240b1e0cfc9d7e482d8f
lorenshadi98 <lorans.hadi@gmail.com>
2021-10-15 15:39:22 -0700

    completed blogly part three along with some refactoring

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
 from flask import Flask, render_template, request, redirect
-from models import db, connect_db, User, Post
+from models import db, connect_db, User, Post, Tag, PostTag
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -12,24 +12,27 @@ connect_db(app)
 db.create_all()
 
 
-@app.route("/")
+@ app.route("/")
 def home():
     return redirect("/users")
 
 
-@app.route("/users")
+@ app.route("/users")
 def users_home():
+    """Shows list of all current users"""
     users = User.query.all()
     return render_template("users-home.html", users=users)
 
 
-@app.route("/users/new")
+@ app.route("/users/new")
 def render_user_form():
+    """Renders form for creating a new user"""
     return render_template("add-user-form.html")
 
 
-@app.route("/users/new", methods=["POST"])
+@ app.route("/users/new", methods=["POST"])
 def add_user():
+    """Handles data and new user creation"""
     # TO DO: get user data from form using request.form
     first_name = request.form["first_name"].capitalize()
     last_name = request.form["last_name"].capitalize()
@@ -41,20 +44,23 @@ def add_user():
     return redirect("/users")
 
 
-@app.route("/users/<int:user_id>/")
-def user_details_page(user_id):
+@ app.route("/users/<int:user_id>/")
+def render_user_details_page(user_id):
+    """Loads user details page based on passed in user id"""
     found_user = User.query.get_or_404(user_id)
     return render_template("user-details-page.html", user=found_user)
 
 
-@app.route("/users/<int:user_id>/edit")
+@ app.route("/users/<int:user_id>/edit")
 def render_user_edit_form(user_id):
+    """Loads user edit form"""
     found_user = User.query.get_or_404(user_id)
     return render_template("user-edit-form.html", user=found_user)
 
 
-@app.route("/users/<int:user_id>/edit", methods=["POST"])
+@ app.route("/users/<int:user_id>/edit", methods=["POST"])
 def edit_user_info(user_id):
+    """Handles user edit data and modification"""
     found_user = User.query.get_or_404(user_id)
     found_user.first_name = request.form["first_name"]
     found_user.last_name = request.form["last_name"]
@@ -63,53 +69,116 @@ def edit_user_info(user_id):
     return redirect(f"/users/{user_id}")
 
 
-@app.route("/users/<int:user_id>/delete/")
+@ app.route("/users/<int:user_id>/delete/")
 def delete_user(user_id):
+    """Handles user deletion"""
     User.query.filter(User.id == user_id).delete()
     db.session.commit()
     return redirect(f"/users")
 
-
-@app.route("/users/<int:user_id>/posts/new")
-def render_user_post_form(user_id):
-    found_user = User.query.get_or_404(user_id)
-    return render_template("new-post-form.html", user=found_user)
+# ==========================POSTS ROUTES ==============================
 
 
-@app.route("/users/<int:user_id>/posts/new", methods=["POST"])
-def create_new_post(user_id):
-    title = request.form["post-title"]
-    content = request.form["post-content"]
-    found_user = User.query.get_or_404(user_id)
-    new_post = Post(title=title, content=content, user=found_user)
-    db.session.add(new_post)
-    db.session.commit()
-    return redirect(f"/users/{user_id}")
-
-
-@app.route("/posts/<int:post_id>/")
-def load_post_details_page(post_id):
+@ app.route("/posts/<int:post_id>/")
+def render_post_details_page(post_id):
+    """Loads post details page"""
     found_post = Post.query.get_or_404(post_id)
     return render_template("post-details-page.html", post=found_post)
 
 
-@app.route("/posts/<int:post_id>/delete")
+@ app.route("/users/<int:user_id>/posts/new", methods=["GET", "POST"])
+def handle_new_post(user_id):
+    """handles new post operation"""
+    if request.method == "GET":
+        found_user = User.query.get_or_404(user_id)
+        tags = Tag.query.all()
+        return render_template("new-post-form.html", user=found_user, tags=tags)
+    else:
+        found_user = User.query.get_or_404(user_id)
+        tag_ids = [int(num) for num in request.form.getlist("tags")]
+        tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
+        new_post = Post(title=request.form["post-title"],
+                        content=request.form["post-content"], user=found_user, tags=tags)
+        # TO DO: Handle tag association with post
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(f"/users/{user_id}")
+
+
+@ app.route("/posts/<int:post_id>/edit",  methods=["GET", "POST"])
+def handle_post_edit(post_id):
+    """handles post edit"""
+    if request.method == "GET":
+        found_post = Post.query.get_or_404(post_id)
+        tags = Tag.query.all()
+        return render_template("post-edit-form.html", post=found_post, tags=tags)
+    else:
+        found_post = Post.query.get_or_404(post_id)
+        found_post.title = request.form["new-post-title"]
+        found_post.content = request.form["new-post-content"]
+        tag_ids = [int(num) for num in request.form.getlist("tags")]
+        found_post.tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
+        db.session.add(found_post)
+        db.session.commit()
+        return redirect(f"/users/{found_post.user_id}")
+
+
+@ app.route("/posts/<int:post_id>/delete")
 def delete_post(post_id):
+    """Handles post deletion"""
     Post.query.filter(Post.id == post_id).delete()
     db.session.commit()
     return redirect("/users")
 
-
-@app.route("/posts/<int:post_id>/edit")
-def render_post_edit_form(post_id):
-    found_post = Post.query.get_or_404(post_id)
-    return render_template("post-edit-form.html", post=found_post)
+# ======================================= TAGS routes ===========================
 
 
-@app.route("/posts/<int:post_id>/edit", methods=["POST"])
-def edit_post_info(post_id):
-    found_post = Post.query.get_or_404(post_id)
-    found_post.title = request.form["new-post-title"]
-    found_post.content = request.form["new-post-content"]
+@ app.route("/tags")
+def all_tags_page():
+    """renders all tags page"""
+    tags = Tag.query.all()
+    return render_template("tags-home-page.html", tags=tags)
+
+
+@ app.route("/tags/<int:tag_id>")
+def render_tag_details_page(tag_id):
+    foundTag = Tag.query.get_or_404(tag_id)
+    return render_template("tags-details-page.html", tag=foundTag)
+
+
+@ app.route("/tags/new", methods=["GET", "POST"])
+def handle_new_tags():
+    if request.method == "GET":
+        return render_template("new-tags-form.html")
+    else:
+        new_tag = Tag(name=request.form["tag-title"].capitalize())
+        db.session.add(new_tag)
+        db.session.commit()
+        return redirect("/tags")
+
+
+@app.route("/tags/<int:tag_id>/edit", methods=["GET", "POST"])
+def handle_tag_edits(tag_id):
+    found_tag = Tag.query.get_or_404(tag_id)
+    if request.method == "GET":
+        return render_template("tags-edit-page.html", tag=found_tag)
+    else:
+        found_tag.name = request.form["tag-name"]
+        post_ids = [int(num) for num in request.form.getlist("posts")]
+        found_tag.posts = Post.query.filter(Post.id.in_(post_ids)).all()
+        db.session.add(found_tag)
+        db.session.commit()
+        return redirect(f"/tags/{tag_id}")
+
+
+@app.route("/tags/<int:tag_id>/delete")
+def handle_tag_delete(tag_id):
+    Tag.query.filter(Tag.id == tag_id).delete()
     db.session.commit()
-    return redirect(f"/users/{found_post.user_id}")
+    return redirect("/tags")
+
+    # TO DO: Create an add tag link inside post details page.
+    # TO DO: Create tag details page and allow to delete, edit current tag.
+    # TO DO: Add a display on the post details page for its associated tags.
+    # TO DO: Add a checkbox style form for different tags to add. As part of the edit post
+    # and add post pages
--- models.py
+++ models.py
@@ -1,8 +1,15 @@
 """Models for Blogly."""
 import datetime
-
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
 db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
 
 
 class User(db.Model):
@@ -18,19 +25,36 @@ class User(db.Model):
 
 class Post(db.Model):
     """Posts model, associated with user"""
-    ___tablename__ = "posts"
+    __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.Text, nullable=False)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(
         db.DateTime,
         nullable=False,
-        default=datetime.datetime.now)
+        default=datetime.datetime. now)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
 
-def connect_db(app):
-    """Connect to database."""
+class Tag(db.Model):
+    """Tag model, usually assoicated with post"""
+    __tablename__ = "tags"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+    posts_table = db.relationship("PostTag", backref="tag")
+    posts = db.relationship(
+        'Post',
+        secondary="posts_tags",
+        backref="tags"
+    )
 
-    db.app = app
-    db.init_app(app)
+
+class PostTag(db.Model):
+    """Model linking post and tags"""
+    __tablename__ = "posts_tags"
+    post_id = db.Column(db.Integer,
+                        db.ForeignKey('posts.id'),
+                        primary_key=True)
+    tag_id = db.Column(db.Integer,
+                       db.ForeignKey("tags.id"),
+                       primary_key=True)
